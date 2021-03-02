f6102eed9c81880ca5d814a1a38567c4d271d6f1
Sbonelo01 <sbonelomkhize15@gmail.com>
2021-03-02 09:53:40 +0200

    used prettier to format

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,30 +1,34 @@
 "use strict";
 
 module.exports = class StringCalculator {
-	
-	add(stringNumber) {
-		// return 0 if String is empty.
-		if (!stringNumber) return 0; 
-	                    
-		if (stringNumber.indexOf("//") == 0) {
-			var delimiter = new RegExp("\n|,|" + stringNumber.substring(2, stringNumber.indexOf('\n'))),
-				subString = stringNumber.substring(stringNumber.indexOf('\n') + 1, stringNumber.length),
-				splitString = subString.split(delimiter);
-	    			console.log(splitString);
-		} else {
-			var splitString = stringNumber.split(/,|\n/);
-		}
+  add(stringNumber) {
+    // return 0 if String is empty.
+    if (!stringNumber) return 0;
 
-		var sum = 0,
-			neg = [];
-		
-		for (var i = 0; i < splitString.length; i++) {
-			if (parseInt(splitString[i]) < 0) neg.push(splitString[i]);
-			else if (splitString[i] < 1001) sum += parseInt(splitString[i]);
-		}
-		if (neg.length > 0){
-			throw new Error('Negetives not allowed' + neg);
-		}
-		return sum;
-	}
-}
+    if (stringNumber.indexOf("//") == 0) {
+      var delimiter = new RegExp(
+          "\n|,|" + stringNumber.substring(2, stringNumber.indexOf("\n"))
+        ),
+        subString = stringNumber.substring(
+          stringNumber.indexOf("\n") + 1,
+          stringNumber.length
+        ),
+        splitString = subString.split(delimiter);
+      console.log(splitString);
+    } else {
+      var splitString = stringNumber.split(/,|\n/);
+    }
+
+    var sum = 0,
+      neg = [];
+
+    for (var i = 0; i < splitString.length; i++) {
+      if (parseInt(splitString[i]) < 0) neg.push(splitString[i]);
+      else if (splitString[i] < 1001) sum += parseInt(splitString[i]);
+    }
+    if (neg.length > 0) {
+      throw new Error("Negetives not allowed" + neg);
+    }
+    return sum;
+  }
+};
